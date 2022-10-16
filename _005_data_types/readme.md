b67b18fc58a696ae04edb04310eaeb033fa5d9df
Jayden <jayden@172.20.nate.com>
2022-10-16 17:44:31 +0900

    Add: 005_data_types

---
+++ _005_data_types/readme.md
@@ -0,0 +1,29 @@
+# Data types in Rust
+---
+
+### Integer types
+
+| Length | Signed | Unsigned |
+|--------|--------|----------|
+| 8-bit  | i8     | u8       |
+| 16-bit | i16    | u16      |
+| 32-bit | i32    | u32      |
+| 64-bit | i64    | u64      |
+| arch   | isize  | usize    |
+
+### Integer literals
+
+| Number literals | Example |
+|-----------------|---------|
+| Decimal         | 98_222  |
+| Hex             | 0xff    |
+| Octal           | 0o77    |
+| Binary          | 0b1111_0000 |
+| Byte (u8 only)  | b'A'    |
+
+### Floating-point types
+
+| Length | Type |
+|--------|------|
+| 32-bit | f32  |
+| 64-bit | f64  |
---
+++ _005_data_types/src/main.rs
@@ -0,0 +1,62 @@
+fn main() {
+    // integer types
+    let a: i8 = 10;         // range: -128 to 127
+    let b: i16 = 10;        // range: -32,768 to 32,767
+    let c: i32 = 10;        // range: -2,147,483,648 to 2,147,483,647
+    let d: i64 = 10;        // range: -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807
+    let e: u8 = 10;         // range: 0 to 255
+    let f: u16 = 10;        // range: 0 to 65,535
+    let g: u32 = 10;        // range: 0 to 4,294,967,295
+    let h: u64 = 10;        // range: 0 to 18,446,744,073,709,551,615
+    let i: isize = 10;      // range: -2^31 to 2^31-1
+    let j: usize = 10;      // range: 0 to 2^32-1
+                            // depending on the architecture of the computer
+                            // in 32-bit architecture, isize and usize are 32-bit
+
+    // floating point types
+    let m: f32 = 10.0;      // range: 1.2E-38 to 3.4E+38
+    let n: f64 = 10.0;      // range: 2.3E-308 to 1.7E+308
+     
+    // boolean type
+    let o: bool = true;
+
+    // character type
+    let p: char = 'a';
+    let q: char = '😂';     // unicode character
+
+    // array type
+    let r: [i32; 5] = [1, 2, 3, 4, 5];
+
+    r[0] == 1;              // true
+    r[1] == 2;              // true
+    r[2] == 3;              // true
+    r[3] == 4;              // true
+    r[4] == 5;              // true
+
+    let s: [i32; 5] = [0; 5];
+
+
+
+    // string type
+    let t: &str = "Hello, world!";
+
+    // tuple type
+    let u: (i32, f64, u8) = (500, 6.4, 1);
+    let (v, w, x) = u;
+
+    u.0 == 500;             // true
+    u.1 == 6.4;             // true
+    u.2 == 1;               // true
+
+    
+
+
+
+
+
+
+
+
+    
+
+}
